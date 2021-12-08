9ad90ae1e4f020709b5c712614bc310069cc3599
gokulnair2001 <gokulnair.2001@gmail.com>
2021-12-08 12:02:41 +0530

    Read/Create added

--- Sources/App/Controllers/SongController.swift
+++ Sources/App/Controllers/SongController.swift
@@ -5,4 +5,25 @@
 //  Created by Gokul Nair on 05/12/21.
 //
 
-import Foundation
+import Fluent
+import Vapor
+
+struct SongController: RouteCollection {
+    
+    func boot(routes: RoutesBuilder) throws {
+        let songs = routes.grouped("songs")
+        songs.get(use: index)
+        songs.post(use: create)
+    }
+    
+    // GET Request /Songs Route
+    func index(req: Request) throws -> EventLoopFuture<[Song]> {
+        return Song.query(on: req.db).all()
+    }
+    
+    // POST Request /Add Songs
+    func create(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        let song = try req.content.decode(Song.self)
+        return song.save(on: req.db).transform(to: .ok)
+    }
+}
